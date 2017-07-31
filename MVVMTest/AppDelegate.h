908d3da3cd42e983fdefb113342aef3629de5e66
guzhiyang <1017574338@qq.com>
2017-07-31 11:34:52 +0800

    Initial Commit

---
+++ MVVMTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVMTest
+//
+//  Created by guzhiyang on 2017/7/31.
+//  Copyright © 2017年 TonyPersonalProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
